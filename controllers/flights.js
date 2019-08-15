40bca90bb177cf34f4364b845df14b3c1a60f653
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-08-15 09:36:18 -0500

    initial commit

---
+++ controllers/flights.js
@@ -0,0 +1,17 @@
+var Flight = require('../models/flight');
+
+module.exports = {
+    index,
+    new: newFlight,
+
+};
+
+function index(req, res) {
+    Flight.find({}, function(err, flights){
+        res.render('flights/index', {flights});
+    });
+}
+
+function newFlight(req, res) {
+    res.render('flights/new');
+};
