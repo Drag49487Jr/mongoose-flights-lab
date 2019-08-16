60f1f5a98284a1a14ecfb8bd164adbd3ef0b5617
Armando Samperio <armandosamperiomcchs@Armandos-MacBook-Pro.local>
2019-08-16 13:40:32 -0500

    add des. show

--- config/db.js
+++ config/db.js
@@ -1,12 +1,12 @@
 var mongoose = require('mongoose');
 
 mongoose.connect(
-    'mongodb://localhost:27017/flights',
+    'mongodb://localhost/flights',
     {useNewUrlParser: true, useCreateIndex:true}
 );
 
 var db = mongoose.connection;
 
 db.on('connected', function(){
-    console.log(`Connected to MongoDB at ${db.host}:${db.port}}`)
+    console.log(`Connected to MongoDB at ${db.host}:${db.port}`)
 })
--- controllers/flights.js
+++ controllers/flights.js
@@ -2,6 +2,7 @@ var Flight = require('../models/flight');
 
 module.exports = {
     index,
+    show,
     new: newFlight,
     create,
 };
@@ -11,19 +12,23 @@ function index(req, res) {
         res.render('flights/index', {title: 'All Flights', flights});
     });
 }
-
+function show(req, res) {
+    Flight.findById(req.params.id, function(err, flight){
+        res.render('flights/show', {title: 'Movie Detail', flight});
+    });
+}
 function newFlight(req, res) {
     res.render('flights/new', {title: 'All Flights'});
 };
 
 function create(req, res) {
-    // for(let key in req.body) {
-    //     if(req.body[key] === '') delete req.body[key];
-    
+    for(let key in req.body) {
+        if(req.body[key] === '') delete req.body[key];
+    }
     var flight = new Flight(req.body)
     flight.save(function(err){
         console.log(flight);
-        if(err) return res.redirect('flights');
-        res.redirect('flights');
+        //if(err) return res.redirect('/flights/new');
+        res.redirect('/flights');
     });
 }
